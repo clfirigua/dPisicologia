15f7c6c000ef2d32e8ceb3d85a22a4bc79f83631
daniel <danielacero@unisangil.edu.co>
2022-02-10 18:51:06 -0500

    pequeños cambios

--- public/shared/js/form.js
+++ public/shared/js/form.js
@@ -15,102 +15,6 @@ let preguntas = [];
 let collection = "Formularios";
 
 
-const boton = (tipo) => {
-    respuestas.innerHTML += `<button class="btn btn-success" id="respuesta"> Respuesta </button>`;
-    const btn = document.getElementById('respuesta');
-    btn.addEventListener('click', (event) => {
-        event.preventDefault();
-        if (tipo) {
-            const multiple = document.getElementById('multiple');
-
-             multiple.innerHTML += `
-                <div class="form-check" id="${cont}" >
-                    <i class="far fa-check-square">
-                        <input type="text" class="inp-bottom-line" placeholder="opcion multiple">
-                        <button class="btn btn-danger data" data-id="${cont}">Eliminar</button>
-                    </i>
-                </div>
-           `
-           const btnDeleteTable = document.querySelectorAll('.data');
-           btnDeleteTable.forEach((btn) =>
-                   btn.addEventListener("click",  (e) => { 
-                       e.preventDefault()
-                        const {id} = e.target.dataset 
-                        const target = document.getElementById(id)
-                        multiple.removeChild(target);
-                        cont-1;
-                   })
-               );
-          cont++
-        } else {
-
-        }
-    })
-
-
-
-}
-
-tipoRespuesta.addEventListener("change", (e) => {
-    let tipo = e.target.value;
-
-    switch (tipo) {
-        case "Respuesta corta":
-            respuestas.innerHTML += `
-            <div class="mb-3">
-                <input type="text" name="respuesta-corta" id="respuesta-corta" class="inp-bottom-line"
-                placeholder="Respuesta corta">
-                <butt
-            </div> `;
-            break;
-        case "Respuesta larga":
-            respuestas.innerHTML += `
-            <div class="mb-3">
-                <textarea name="" id="" rows="5" placeholder="Respuesta larga"></textarea>
-            </div>`;
-            break;
-        case "Opcion multiple":
-            respuestas.innerHTML += `
-            <div class="mb-3" id="multiple">
-                <label for="opcionmultiple" class="col-form-label">Opcion multiple</label>
-
-            </div>`
-            boton(true);
-            break;
-        case "Opcion unica":
-            respuestas.innerHTML += `
-            <div class="mb-3">
-            <label for="opcion-unica" class="col-form-label">Opcion unica</label>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" disabled name="flexRadioDisabled" id="flexRadioDisabled">
-                    <input type="text" class="inp-bottom-line" placeholder="opcion unica">
-                    </label>
-                </div>
-            </div>`
-            boton(false);
-            break;
-        default:
-            respuestas.innerHTML += `
-                <p>Selecciona un tipo de respuesta</p>
-            `
-            break;
-    }
-})
-
-butttom.addEventListener('click', (event)=>{
-    event.preventDefault();
-    for(let i=0; i< cont; i++){
-          const data = document.getElementById(i);
-            if(data?.children[0]?.children[0]?.value  != undefined){
-                const pregunta = data?.children[0]?.children[0]?.value
-                preguntas.push({pregunta})
-            }
-          
-          
-    }
-    console.log(preguntas)
-})
-
 
 
 window.addEventListener("DOMContentLoaded", async (e) => {
@@ -145,4 +49,123 @@ window.addEventListener("DOMContentLoaded", async (e) => {
         }
     }
 });
+
+
+
+const boton = (tipo) => {
+    respuestas.innerHTML += `<button class="btn btn-success" id="respuesta"> Respuesta </button>`;
+    const btn = document.getElementById('respuesta');
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (tipo == "Opcion multiple") {
+            const multiple = document.getElementById('multiple');
+
+            multiple.innerHTML += `
+                <div class="form-check opcion-respuesta" id="${cont}" >
+                    <input type="text" class="inp-bottom-line" placeholder="opcion multiple">
+                    <button class="btn btn-danger eliminar" data-id="${cont}">Eliminar</button>
+                </div>
+           `
+            const btnDeleteTable = document.querySelectorAll('.eliminar');
+            btnDeleteTable.forEach((btn) =>
+                btn.addEventListener("click", (e) => {
+                    e.preventDefault()
+                    const { id } = e.target.dataset
+                    console.log(id)
+                    const target = document.getElementById(id)
+                    multiple.removeChild(target);
+                    cont - 1;
+                })
+            );
+            cont++
+        } else {
+            if (tipo == "Opcion unica") {
+                const unica = document.getElementById("unica");
+                unica.innerHTML += `
+                <div class="form-check opcion-respuesta" id="${cont}">
+                    <input type="text" class="inp-bottom-line" placeholder="opcion unica">
+                    <button class="btn btn-danger eliminar" data-id="${cont}">Eliminar</button>
+                </div>`
+                const btnDeleteTable = document.querySelectorAll('.eliminar');
+                btnDeleteTable.forEach((btn) =>
+                    btn.addEventListener("click", (e) => {
+                        e.preventDefault()
+                        const { id } = e.target.dataset
+                        const target = document.getElementById(id)
+                        unica.removeChild(target);
+                        cont - 1;
+                    })
+                );
+                cont++
+
+            }
+
+        }
+    })
+
+
+
+}
+
+tipoRespuesta.addEventListener("change", (e) => {
+    let tipo = e.target.value;
+    respuestas.innerHTML = ""
+    switch (tipo) {
+        case "Respuesta corta":
+            respuestas.innerHTML += `
+            <div class="mb-3">
+                <input type="text" name="respuesta-corta" id="respuesta-corta" class="inp-bottom-line"
+                placeholder="Respuesta corta">
+                <butt
+            </div> `;
+            break;
+        case "Respuesta larga":
+            respuestas.innerHTML += `
+            <div class="mb-3">
+                <textarea name="" id="" rows="5" placeholder="Respuesta larga"></textarea>
+            </div>`;
+            break;
+        case "Opcion multiple":
+            respuestas.innerHTML += `
+            <div class="mb-3" id="multiple">
+                <label for="opcionmultiple" class="col-form-label">Opcion multiple</label>
+            </div>`
+            boton(tipo);
+            break;
+        case "Opcion unica":
+            respuestas.innerHTML += `
+            <div class="mb-3" id="unica">
+                <label for="opcionunica" class="col-form-label">Opcion unica</label>
+            </div>`
+            boton(tipo);
+            break;
+        default:
+            respuestas.innerHTML += `
+                <p>Selecciona un tipo de respuesta</p>
+            `
+            break;
+    }
+})
+
+butttom.addEventListener('click', async (e) => {
+    e.preventDefault();
+    let id = localStorage.getItem("idAddForm")
+    let objetRespuestas
+    for (let i = 0; i < cont; i++) {
+        const data = document.getElementById(i);
+        if (data?.children[0]?.value != undefined) {
+            objetRespuestas = [data?.children[0]?.value]
+            objetRespuestas.push(objetRespuestas)
+        }
+    }
+    await updateDocument(collection, id, {
+        pregunta: pregunta.value,
+        tipoRespuesta: tipoRespuesta.value,
+        preguntaDepende: preguntaDepende.value,
+        preguntaDependiente: preguntaDependiente.value,
+        respuestaDepende: respuestaDepende.value,
+        respuestas: ["dads","das",2],
+
+    })
+})
 
