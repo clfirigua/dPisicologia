d4eeda21626804c9aafa1129d590612e621c0bf0
aceros123 <57160341+aceros123@users.noreply.github.com>
2022-03-04 14:32:51 -0500

    actualizacion
    luego de dañar todo, por tener versiones diferentes en dos sistemas, se intento restablecer los cambios a  como estaban con una copia de seguridad local del 2 de marzo

--- public/shared/js/forms.js
+++ public/shared/js/forms.js
@@ -1,26 +1,27 @@
 
-import { addDocument, onGetDocuments, deleteDocuments, getDocument, oneDoc } from "./crudFirebase.js";
+import { addDocument, onGetDocuments, deleteDocuments, getDocument, oneDoc} from "./crudFirebase.js";
 const addForm = document.getElementById("add-form");
 const nameForm = document.getElementById("name-form");
-const tableForm = document.getElementById("forms-table");
+const tableForm =  document.getElementById("forms-table");
 let collection = "Formularios"
 
 
-addForm.addEventListener("click", async (e) => {
-    const document = await oneDoc(collection, nameForm.value, {
+addForm.addEventListener("click", async ()=>{
 
+   const document = await oneDoc(collection,nameForm.value,{
+        
     })
-    localStorage.setItem("idAddForm", nameForm.value)
-    localStorage.setItem("estado", "creando");
+    localStorage.setItem("idAddForm",nameForm.value)
+    localStorage.setItem("estado","creando");
     location.href = "./formulario.html"
 })
 
 window.addEventListener('DOMContentLoaded', async (e) => {
-    onGetDocuments(collection, (querySnapshot) => {
-        tableForm.innerHTML = ""
-        querySnapshot.forEach((doc) => {
+    onGetDocuments(collection,(querySnapshot) =>{
+        tableForm.innerHTML=""
+        querySnapshot.forEach((doc)=>{
             const data = doc.data();
-            tableForm.innerHTML += `
+            tableForm.innerHTML+=`
             <tr>
                 <td scope="row" class="text-center">${doc.id}</td>
                 <td scope="row" class="text-center">${data.descripcion}</td>
@@ -29,12 +30,12 @@ window.addEventListener('DOMContentLoaded', async (e) => {
             </tr>
             `
         })
-
+        
         const btnDeleteForm = document.querySelectorAll('.delete-form');
         const btnUpdateForm = document.querySelectorAll('.update-form');
 
 
-        btnDeleteForm.forEach((btn) => {
+        btnDeleteForm.forEach((btn)=>{
             btn.addEventListener("click", async ({ target: { dataset } }) => {
                 try {
                     await deleteDocuments(collection, dataset.id);
@@ -44,15 +45,15 @@ window.addEventListener('DOMContentLoaded', async (e) => {
                 }
             })
         })
-        btnUpdateForm.forEach((btn) => {
-            btn.addEventListener("click", async (e) => {
-                try {
-                    const doc = await getDocument(collection, e.target.dataset.id)
+        btnUpdateForm.forEach((btn)=>{
+            btn.addEventListener("click",async(e)=>{
+                try{
+                    const doc =await getDocument(collection,e.target.dataset.id)
                     const form = doc.data();
-                    localStorage.setItem('idUpdateForm', doc.id);
-                    localStorage.setItem("estado", "editando");
+                    localStorage.setItem('idUpdateForm',doc.id);
+                    localStorage.setItem("estado","editando");
                     location.href = "./formulario.html"
-                } catch (error) {
+                }catch(error){
                     console.log(error)
                 }
             })
